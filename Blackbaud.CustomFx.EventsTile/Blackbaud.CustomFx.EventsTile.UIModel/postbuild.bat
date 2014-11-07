001849654fbc43ac08cc614ff95bae04f0d4edc9
blackbaud-johnly <John.Lyons@blackbaud.com>
2014-11-07 14:56:08 -0500

    Update path in .bat file

--- Blackbaud.CustomFx.EventsTile/Blackbaud.CustomFx.EventsTile.UIModel/postbuild.bat
+++ Blackbaud.CustomFx.EventsTile/Blackbaud.CustomFx.EventsTile.UIModel/postbuild.bat
@@ -1,5 +1,5 @@
 echo copy the html and js files to the deploy folder on the webserver...
-xcopy "%~dp0htmlforms\*.*" "C:\Program Files\Blackbaud\bbappfx\vroot\browser\htmlforms\custom" /e /y /r
+xcopy "%~dp0htmlforms\*.*" "C:\Program Files\Blackbaud\bbappfx\vroot\browser\htmlforms" /e /y /r
 
 rem echo minify the html and js files to optimize their payload on the wire...
 rem %~dp0..\..\..\..\Utils\Blackbaud.AppFx.JSMinifier\bin\JSMinifier.exe %~dp0..\..\..\..\Blackbaud.AppFx.Server\Deploy\browser\htmlforms\<subfolder>\*.html /pre
